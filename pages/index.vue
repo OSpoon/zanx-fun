5c17787a3458a11f810cbe11c2cb3c3b21c94f92
Clacky <develop@clacky.ai>
2025-03-09 09:30:46 -0400

    Fix: Remove redundant article link and fix typo in highlight.js

--- pages/index.vue
+++ pages/index.vue
@@ -445,17 +445,6 @@ const visiblePageNumbers = computed(() => {
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                         </svg>
                       </button>
-                      <a 
-                        v-if="article.originalUrl" 
-                        :href="article.originalUrl" 
-                        target="_blank" 
-                        class="text-primary hover:underline flex items-center"
-                      >
-                        <span>{{ $t('blog.readMore') }}</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                        </svg>
-                      </a>
                     </div>
                   </div>
                 </div>
